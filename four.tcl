4d5a16739e7b86632d2b7049151c264fa36ff118
avani1998 <46353367+avani1998@users.noreply.github.com>
2019-10-23 23:39:06 +0530

    Update four.tcl

--- four.tcl
+++ four.tcl
@@ -1,6 +1,6 @@
-#Q4 Stimulate the transmission of ping messages over a network topology consisting of 6 nodes and find the number of dropped packets due to congession
-
-set ns[new Simulator]
+#Q4 Stimulate the transmission of ping messages over a network topology consisting of 6 nodes and 
+#find the number of dropped packets due to congession
+set ns [new Simulator]
 
 #set trace file
 set traceFile [open 4.tr w]
@@ -8,7 +8,7 @@ $ns trace-all $traceFile
 set namFile [open 4.nam w]
 $ns namtrace-all $namFile
 
-proc finish{}	{
+proc finish {}	{
 	global ns namFile traceFile
 	$ns flush-trace 
 	close $traceFile
@@ -17,21 +17,21 @@ proc finish{}	{
 		exec nam 4.nam &
 	exit 0
 
-]
+}
 
 #create 4 nodes 
-for{set i 0} {$i<7} {incr i} {
+for {set i 0} {$i < 7} {incr i} {
 	set n($i) [$ns node]
 }
 
-#craete linkes betweeen the nodes 
-for {set i 1} {$i<4} {incr i} {
-	$ns duplex-link $n($i) $n(0) 1Mb 10ms Droptail
+#create linkes betweeen the nodes 
+for {set i 1} {$i < 4} {incr i} {
+	$ns duplex-link $n($i) $n(0) 1Mb 10ms DropTail
 	
 }
-$ns duples-link $n(4) $n(0) 1Mb 100ms Droptail
-$ns duples-link $n(5) $n(0) 1Mb 100ms Droptail
-$ns duples-link $n(6) $n(0) 1Mb 90ms Droptail
+$ns duplex-link $n(4) $n(0) 1Mb 100ms DropTail
+$ns duplex-link $n(5) $n(0) 1Mb 100ms DropTail
+$ns duplex-link $n(6) $n(0) 1Mb 90ms DropTail
 
 Agent/Ping instproc recv {from rtt} {
 	$self instvar node_
@@ -40,11 +40,11 @@ Agent/Ping instproc recv {from rtt} {
 }
 
 #create ping agents 
-for{set i 1} {$i<7} {incr i} {
+for {set i 1} {$i<7} {incr i} {
 	set pingAgent($i) [new Agent/Ping]
 		$ns attach-agent $n($i) $pingAgent($i)
 }
-for{set i 1} {$i<6} {incr i} {
+for {set i 1} {$i<6} {incr i} {
 	$ns queue-limit $n(0)  $n($i) 2
 }
 
