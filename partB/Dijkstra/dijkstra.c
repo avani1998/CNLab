235c332eea7e58cd1234562626400db55719c4e4
avani1998 <46353367+avani1998@users.noreply.github.com>
2019-11-21 09:35:38 +0000

    Create dijkstra.c

---
+++ partB/Dijkstra/dijkstra.c
@@ -0,0 +1,76 @@
+Aishwarya Sheelvant
+	
+2:55 PM (8 minutes ago)
+	
+to me
+
+
+---------- Forwarded message ---------
+From: bhoomika chandrashekar <bhoomi.chandrashekar@gmail.com>
+Date: Thu, Nov 21, 2019 at 2:46 PM
+Subject: dijkstra
+To: <anaghayadav279@gmail.com>, <Aishwaryavs12@gmail.com>, Akhila Raj <akhilaraj474@gmail.com>, <aishwaryamanikarnike@gmail.com>
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#define max 10
+
+void dijkstras(int source, int cost[max][max], int visited[max], int d[max], int n)
+{
+	int i, j, min, v, w;
+	for(i=1; i<=n; i++)
+	{
+		visited[i]=0;
+		d[i]=cost[source][i];
+	}
+	visited[source]=1;
+	d[source]=0;
+	for(j=2; j<=n; j++)
+	{
+		min=999;
+		for(i=1; i<=n; i++)
+		{
+			if(!visited[i])
+			{
+				if(d[i]<min)
+				{
+					min=d[i];
+					v=i;
+				}
+			}
+		}
+		visited[v]=1;
+		for(w=1; w<=n; w++)
+		{
+			if(cost[v][w]!=999 && visited[w]==0)
+			{
+				if(d[w]>cost[v][w]+d[v])
+					d[w]=cost[v][w]+d[v];
+			}
+		}
+	}
+}
+
+void main()
+{
+	int n, cost[max][max], i, j, source, visited[max], d[max];
+	printf("Enter the number of elements\n");
+	scanf("%d",&n);
+	printf("Enter the adjacency matrix\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			scanf("%d",&cost[i][j]);
+		}
+	}
+	printf("Enter the starting node\n");
+	scanf("%d",&source);
+	dijkstras(source, cost, visited, d, n);	
+	for(i=1; i<=n; i++)
+	{
+		if(i!=source)
+			printf("\nShortest path from %d to %d => %d\n",source, i, d[i]);
+	}
+}
